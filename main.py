f1b54f5f40618215e1f8db7fb6e7b81997428aba
Pedro <pedrobgarcia1104@gmail.com>
2025-09-04 23:42:20 -0300

    Add CORS middleware configuration to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.database import create_db_and_tables
 from contextlib import asynccontextmanager
 from routers import login_router, authentication_router
@@ -10,8 +11,18 @@ async def lifespan(app: FastAPI):
     yield  
     print("API sendo encerrada...")
 
+
+
 app = FastAPI(lifespan=lifespan)
 
-#app.include_router(jwt_router.router)
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(login_router.router)
 app.include_router(authentication_router.router)
