c078226aefd7a7d6ee00434af347008c813f374a
Gabriele <gabriele.bernardo.oliveira@gmail.com>
2025-09-08 11:13:35 -0300

    first version with all features inegrated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from core.database import create_db_and_tables
-from routers import line_router, city_router, schedule_router, bus_router
+from routers import line_router, city_router, schedule_router, bus_router, user_router, authentication_router
 import uvicorn, os
 
 @asynccontextmanager
@@ -27,6 +27,8 @@ app.include_router(line_router.router)
 app.include_router(city_router.router)
 app.include_router(schedule_router.router)
 app.include_router(bus_router.router)
+app.include_router(user_router.router)
+app.include_router(authentication_router.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
---
+++ routers/authentication_router.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from schemas import jwt_schema
+from repository import jwt_repo
+from datetime import timedelta
+from core.database import SessionDep
+from config import ACCESS_TOKEN_EXPIRE_MINUTES
+
+router = APIRouter(tags=["Login"])
+
+@router.post("/login")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: SessionDep) -> jwt_schema.Token:
+
+    user = jwt_repo.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"},)
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = jwt_repo.create_access_token(
+        data={"sub": user.name}, expires_delta=access_token_expires)
+
+    return jwt_schema.Token(access_token=access_token, token_type="bearer")
